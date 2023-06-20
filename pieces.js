f9f70dd3fa73952c577be2a8d7e2a19583192aef
Julien Sailly <juliensailly76@gmail.com>
2023-06-20 12:16:39 +0200

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -1,6 +1,7 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
+
 // Création des balises 
 const article = pieces[0];
 const imageElement = document.createElement("img");
@@ -11,9 +12,16 @@ const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
+const enStockElement = document.createElement("p");
+enStockElement.innerText = article.disponibilite ? "En stock" : "Epuisé...";
+
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(enStockElement);
