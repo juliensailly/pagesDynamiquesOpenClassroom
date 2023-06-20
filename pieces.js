fa50c9b14944ea36e7d0058798af8ec9c64f59fe
Julien Sailly <juliensailly76@gmail.com>
2023-06-20 12:28:28 +0200

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -36,3 +36,18 @@ for (let i = 0; i < pieces.length; i++) {
 
  }
  
+ const boutonTrier = document.querySelector(".btn-trier");
+ boutonTrier.addEventListener("click", function() {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+ });
+
+ const boutonFilter = document.querySelector(".btn-filter");
+ boutonFilter.addEventListener("click", function() {
+    const piecesFiltrees = pieces.filter(function(piece) {
+        return piece.prix <= 35;
+    });
+ });
